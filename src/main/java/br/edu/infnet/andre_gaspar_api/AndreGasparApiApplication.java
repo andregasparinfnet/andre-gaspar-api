15c5c55b8d2902a206b8979bf5b6520d0f23f8bd
Andre Gaspar <andre.gaspar@al.infnet.edu.br>
2026-08-27 18:33:46 -0300

    Projeto Spring Boot inicial

---
+++ src/main/java/br/edu/infnet/andre_gaspar_api/AndreGasparApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.andre_gaspar_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndreGasparApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndreGasparApiApplication.class, args);
+	}
+
+}
